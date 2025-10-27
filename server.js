3f8233643b3be5a1daebed90d217b3eb396fd4ba
heric-hh <erhh.015@gmail.com>
2025-10-27 07:35:46 -0600

    cambios

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hola Mundo desde Express.js, listo para DevOps Build!");
 });
 
+app.get("/hola", (req, res) => {
+  res.send("saludos");
+});
+
 app.listen(PORT, () => {
   console.log(`Aplicación Express escuchando en http://localhost:${PORT}`);
 });
