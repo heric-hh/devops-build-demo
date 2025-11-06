644a58307732c08e43e87c2598038df222922b59
heric-hh <erhh.015@gmail.com>
2025-11-05 20:08:24 -0600

    cambios extra

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("¡Hola CI/CD Build Exitoso y mas cambios!");
+  res.send("¡Hola CI/CD Build Exitoso!");
 });
 
 // Solo escucha el puerto si el módulo se ejecuta directamente (no cuando se importa para pruebas)
